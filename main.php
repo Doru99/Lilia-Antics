429d8beb72345c19744154f11e9c28b14dc1ebde
Doru99 <32598341+Doru99@users.noreply.github.com>
2020-08-22 15:55:01 +0300

    Add files via upload

--- main.php
+++ main.php
@@ -2,9 +2,30 @@
     
 <head>
     <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="myMenu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="card.css">
 </head>
 
 <body style="margin:0; padding:0;">
+
+    <div id="meniu_container">
+        <div class="search-container">
+            <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+        <nav class="meniu">
+            <ul class="main_meniu">
+                <li class="active"><a href="main.php" class="meniu_page">Home</a></li>
+                <li><a href="produse.php" class="meniu_page">Produse</a></li>
+                <li><a href="cos.php" class="meniu_page">Cos</a></li> 
+            </ul>
+        </nav>
+    </div>
+
+
     <div id="container">
         <?php
             $connection=mysqli_connect("localhost", "root","");
@@ -13,56 +34,85 @@
             $query_run=mysqli_query($connection,$query);
             while ($row=mysqli_fetch_array($query_run))
             {
-                echo $row['idProdus'];
                 ?>
-                <br>
-                <?php
-                echo $row['descriere'];
-                ?>
-                <br>
-                <?php
-                echo '<img src="data:image;base64,'.base64_encode($row['poza']).'"alt="Poza">';
-                ?>
-                <br>
+                <div class="card">
+                  <div class="img_prod">
+                  <?php
+                  echo '<img src="data:image;base64,'.base64_encode($row['poza']).'"alt="Poza">';
+                  ?>
+                  </div>
+                  <div class="info_prod">
+                    <div class="text_prod">
+                      <span class="numProd"><?php echo $row['numProdus'];?></span>
+                      <span class="tipProd"><?php echo $row['tip'];?></span>
+                      <span class="descProd"><?php echo $row['descriere'];?></span>
+                    </div>
+                    <div class="com_prod">
+                      <span class="pretProd"><?php echo $row['pret'];?> lei</span>
+                      <br>
+                      <button type="button">Adauga in cos</button>
+                    </div>
+                  </div>
+                </div>
                 <?php
             }
         ?>
     </div>
     
     <div id="footer">
-        <div class="footer_comp">
-            <span class="footer_titlu">Lilia Antique</span>
-            <hr>
-            <br>
-            <span class="descriere_comp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </span>
+      <div class="footer_box">
+        <span class="footer_titlu">Lilia Antique</span>
+        <hr>
+        <br>
+        <span class="descriere_comp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </span>
+      </div>
+      <div class="footer_box">
+        <span class="footer_titlu">Linkuri utile</span>
+        <hr>
+        <br>
+        <div class="contact_link">
+          <a href="main.html" class="footer_link">Despre noi</a>
         </div>
-        <div class="footer_produs">
+        <div class="contact_link">
+          <a href="produse.php" class="footer_link">Produse</a>
         </div>
-        <div class="footer_contact">
-            <span class="footer_titlu">Contact</span>
-            <hr>
-            <br>
-            <div class="contact_link">
-                <img src="imagini\fb.svg" alt="fb" class="icon">
-                <a href="https://www.facebook.com/liliantiqueshop">Lilia Antique</a>
-            </div>
-            <div class="contact_link">
-                <img src="imagini\gmail.svg" alt="gmail" class="icon">
-                <span class="contact_text">oprea.stefanteodor@gmail.com</span>
-            </div>
-            <div class="contact_link">
-                <img src="imagini\tel.svg" alt="tel" class="icon">
-                <span class="contact_text"> +40 722 900 252</span>
-            </div>
-            <div class="contact_link">
-                <img src="imagini\address.svg" alt="adr" class="icon">
-                <span class="contact_text">strada Grigore Plesoianu, bl. 11B, et. 1</span>
-            </div>
+        <div class="contact_link">
+          <a href="cos.php" class="footer_link">Cos</a>
         </div>
+      </div>
+      <div class="footer_box">
+        <span class="footer_titlu">Contact</span>
+        <hr>
+        <br>
+        <div class="contact_link">
+          <img src="imagini\fb.svg" alt="fb" class="icon_contact">
+          <a href="https://www.facebook.com/liliantiqueshop" class="footer_link">Lilia Antique</a>
+        </div>
+        <div class="contact_link">
+          <img src="imagini\gmail.svg" alt="gmail" class="icon_contact">
+          <span style="word-break: break-all;" class="contact_text">oprea.stefanteodor@gmail.com</span>
+        </div>
+        <div class="contact_link">
+          <img src="imagini\tel.svg" alt="tel" class="icon_contact">
+          <span class="contact_text"> +40 722 900 252</span>
+        </div>
+        <div class="contact_link">
+          <img src="imagini\address.svg" alt="adr" class="icon_contact">
+          <span class="contact_text">strada Grigore Plesoianu, bl. 11B, et. 1</span>
+        </div>
+      </div>
+      <div class="footer_box">
+        <span class="footer_titlu">Metode de plata</span>
+        <hr>
+        <br>
+        <img src="imagini\paypal.svg" alt="paypal" class="icon_card">
+        <img src="imagini\mastercard.svg" alt="mastercard" class="icon_card">
+        <img src="imagini\visa.svg" alt="visa" class="icon_card">
+      </div>
     </div>
 
 </body>
